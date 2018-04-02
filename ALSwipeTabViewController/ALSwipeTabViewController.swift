b8f319c99e9410959f125137c85396c5f4b0fa7a
ncnagi <ncnagi0@gmail.com>
2018-04-02 18:50:27 +0900

    SwipeTabViewをStoryboardに対応.

--- ALSwipeTabViewController/ALSwipeTabViewController.swift
+++ ALSwipeTabViewController/ALSwipeTabViewController.swift
@@ -14,13 +14,13 @@ class ALSwipeTabViewController: ButtonBarPagerTabStripViewController {
 	internal var swipeTabContentViewControllers = [ALSwipeTabContentViewController]()
 	
 	init() {
-		print("ALSwipeTabViewController:viewDidLoad")
+		print("ALSwipeTabViewController:init")
 		
 		super.init(nibName: nil, bundle: nil)
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+		super.init(coder: aDecoder)
 	}
 	
 	override func viewDidLoad() {
